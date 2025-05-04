0714dd806ac9f1f47454886b04606bdc1c82f6a1
kkwatts <kkwatts15@gmail.com>
2025-05-04 16:11:50 -0500

    Added merge behavior
    1. Add "fruit" tag to original fruit prefab
    2. Add "merge behavior" parts to FruitBehavior
    3. Assign fruitType values to each prefab according to the order they should be able to merge by (ex. 1 = smallest fruit, 10 = biggest)
    4. Assign all of the fruit prefabs in their corresponding fruitType order to the fruits[] array of the original fruit prefab

--- Assets/Scripts/FruitBehavior.cs
+++ Assets/Scripts/FruitBehavior.cs
@@ -3,6 +3,10 @@ using UnityEngine;
 using System.Collections;
 
 public class FruitBehavior : MonoBehaviour {
+    // Merge behavior
+    public int fruitType;
+    public GameObject[] fruits;
+
     // Game over
     private float timeLimit;
     private float gameOverTimer;
@@ -44,5 +48,21 @@ public class FruitBehavior : MonoBehaviour {
     // Display game over text
     private void DisplayGameOver() {
         GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerBehavior>().GameOver();
+    }
+
+    // Merge behavior
+    private void OnCollisionEnter2D(Collision2D col) { 
+        if (col.gameObject.CompareTag("Fruit")) {
+            GameObject otherFruit = col.gameObject;
+            if (fruitType == otherFruit.GetComponent<FruitBehavior>().fruitType && fruitType != 10) {
+                if (transform.position.x > otherFruit.transform.position.x || (transform.position.y > otherFruit.transform.position.y && transform.position.x == otherFruit.transform.position.x)) {
+                    GameObject newFruit = Instantiate(fruits[fruitType], Vector3.Lerp(transform.position, otherFruit.transform.position, 0.5f), Quaternion.identity);
+                    newFruit.GetComponent<CircleCollider2D>().enabled = true;
+                    newFruit.GetComponent<Rigidbody2D>().gravityScale = 1f;
+                    Destroy(otherFruit);
+                    Destroy(gameObject);
+                }
+            }
+        }
     }
 }
