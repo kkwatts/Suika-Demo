e18d79b610b5b06d92c7c9c4cd6a25f939314f22
kkwatts <kkwatts15@gmail.com>
2025-05-04 14:26:07 -0500

    Added game over condition
    1. Create new square sprite, adjust it so it forms the top of the box formed by the wall and floor sprites, give it an "isTrigger" BoxCollider2D, and assign it a tag that marks it as being a game over state, something like "Limit" or "Game Over"
    2. Discuss tags
    3. Create FruitBehavior script and add to original fruit prefab
    4. Add "game over" parts to FruitBehavior
    5. Change Rigidbody2D of original fruit prefab to never sleep

---
+++ Assets/Scripts/FruitBehavior.cs
@@ -0,0 +1,42 @@
+using UnityEngine;
+// Game over
+using System.Collections;
+
+public class FruitBehavior : MonoBehaviour {
+    // Game over
+    private float timeLimit;
+    private float gameOverTimer;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start() {
+        // Game over
+        timeLimit = 2f;
+        gameOverTimer = 0f;
+    }
+
+    // Update is called once per frame
+    void Update() {
+        
+    }
+
+    // Game over
+    private IEnumerator Wait(float seconds) {
+        yield return new WaitForSeconds(seconds);
+        if (gameOverTimer >= timeLimit) {
+            Debug.Log("Game over");
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D col) { 
+        if (col.gameObject.CompareTag("Game Over")) {
+            gameOverTimer += Time.deltaTime;
+            StartCoroutine(Wait(timeLimit));
+        }
+    }
+
+    private void OnTriggerStay2D(Collider2D col) {
+        if (col.gameObject.CompareTag("Game Over")) {
+            gameOverTimer += Time.deltaTime;
+        }
+    }
+}
